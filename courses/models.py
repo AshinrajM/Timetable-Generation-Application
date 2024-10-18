049d597a5cc9db7ffbce6bd54197380a54eb9728
AshinrajM <ashinrajeevank@outlook.com>
2024-10-18 18:07:58 +0530

    timetable app

---
+++ courses/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=100)
+    course = models.ForeignKey(
+        Course, related_name="subjects", on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Staff(models.Model):
+    name = models.CharField(max_length=100)
+    subjects = models.ManyToManyField(Subject)
+
+    def __str__(self):
+        return self.name
+
+
+class Day(models.Model):
+    DAYS_OF_WEEK = [
+        ("MON", "Monday"),
+        ("TUE", "Tuesday"),
+        ("WED", "Wednesday"),
+        ("THU", "Thursday"),
+        ("FRI", "Friday"),
+    ]
+    name = models.CharField(max_length=3, choices=DAYS_OF_WEEK, unique=True)
+
+    def __str__(self):
+        return self.name
+
